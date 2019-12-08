97e1b14b467f1cf3223f789e077305731f48e587
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-12-08 12:01:08 +0100

    Test template contact

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 
 @section('contenu')
-    <br>
+  
 	<div class="col-sm-offset-3 col-sm-6">
 		<div class="panel panel-info">
 			<div class="panel-heading">Contactez-moi</div>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,23 +1,23 @@
 @extends('template')
 
-@section('contenu')
-    <br>
-			<h1>Contactez-moi</h1>
+<h1>Contactez-moi</h1>
 
-				{!! Form::open(['url' => 'contact']) !!}
-					<div class="form-group {!! $errors->has('nom') ? 'has-error' : '' !!}">
-						{!! Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'Votre nom']) !!}
-						{!! $errors->first('nom', '<small class="help-block">:message</small>') !!}
-					</div>
-					<div class="form-group {!! $errors->has('email') ? 'has-error' : '' !!}">
-						{!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Votre email']) !!}
-						{!! $errors->first('email', '<small class="help-block">:message</small>') !!}
-					</div>
-					<div class="form-group {!! $errors->has('texte') ? 'has-error' : '' !!}">
-						{!! Form::textarea ('texte', null, ['class' => 'form-control', 'placeholder' => 'Votre message']) !!}
-						{!! $errors->first('texte', '<small class="help-block">:message</small>') !!}
-					</div>
-					{!! Form::submit('Envoyer !', ['class' => 'btn btn-info pull-right']) !!}
-				{!! Form::close() !!}
+@section('contenu')
+
+	{!! Form::open(['url' => 'contact']) !!}
+		<div class="form-group {!! $errors->has('nom') ? 'has-error' : '' !!}">
+			{!! Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'Votre nom']) !!}
+			{!! $errors->first('nom', '<small class="help-block">:message</small>') !!}
+		</div>
+		<div class="form-group {!! $errors->has('email') ? 'has-error' : '' !!}">
+			{!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Votre email']) !!}
+			{!! $errors->first('email', '<small class="help-block">:message</small>') !!}
+		</div>
+		<div class="form-group {!! $errors->has('texte') ? 'has-error' : '' !!}">
+			{!! Form::textarea ('texte', null, ['class' => 'form-control', 'placeholder' => 'Votre message']) !!}
+			{!! $errors->first('texte', '<small class="help-block">:message</small>') !!}
+		</div>
+		{!! Form::submit('Envoyer !', ['class' => 'btn btn-info']) !!}
+	{!! Form::close() !!}
 
 @endsection
