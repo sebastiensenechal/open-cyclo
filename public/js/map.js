0ed5cb51a4812d4e52cc774cbcff93b6b2e6db26
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-12-12 17:59:47 +0100

    Add Leaflet and OpenCycleMap to home view

---
+++ public/js/map.js
@@ -0,0 +1,75 @@
+// initialisation de la carte OpenStreetMap et Leaflet
+
+
+var mapCyclo = null;
+var mapLayer = null;
+
+var Maps = {
+	lat: 48.862725,
+	lon: 2.287592,
+
+	initMap : function(position) {
+		mapCyclo = L.map(document.getElementById('map'), {scrollWheelZoom:true}).setView([this.lat, this.lon], 13);
+
+		mapLayer = L.tileLayer("https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=2ee1c74c95c54dd7b150e8f4604e7865", {
+			attribution: 'Maps © <a href="http://www.thunderforest.com/" target="_blank">Thunderforest</a>, Data © <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors.</a>',
+			minZoom: 8,
+			maxZoom: 22
+		}).addTo(mapCyclo);
+	},
+
+  // Méthode d'initialisation des markers : Récupère les données stockées dans la variable "listStations"
+  initMarkers : function(latitude, longitude) {
+      markers = L.marker([latitude, longitude]).addTo(mapCyclo);
+      markers.bindPopup("<b>Hello world!</b><br>I am a popup.");
+  }
+
+}
+
+
+
+
+
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    document.getElementById("status").style.display = "none";
+
+    mapCyclo.setView([latitude, longitude], 17);
+    Maps.initMarkers(latitude, longitude);
+
+    console.log(`Latitude: ${latitude} °, Longitude: ${longitude} °`);
+  }
+
+  function error() {
+    status.textContent = 'Impossible de vous localiser';
+  }
+
+  var geo_options = {
+    enableHighAccuracy: true,
+    maximumAge        : 30000,
+    timeout           : 27000
+  }
+
+  var wpid = navigator.geolocation.watchPosition(success, error, geo_options);
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation n\'est pas supporté sur votre navigateur';
+  } else {
+    document.getElementById("status").style.display = "block";
+
+    status.textContent = 'Recherche en cours...';
+
+    // navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.watchPosition(success, error, geo_options);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,9 @@
     			{{ Html::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') }}
     			{{ Html::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') }}
     		<![endif]-->
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+        crossorigin=""/>
     </head>
     <body id="home">
 
@@ -28,6 +31,7 @@
 
 
         <main>
+          <div id ="status"></div>
           <!-- <section id="map-infos">
             <h2>Informations</h2>
             <article>
@@ -43,7 +47,7 @@
         <footer>
           <nav id="toolbar">
             <ul>
-              <li><a href="#"><i class="material-icons">location_searching</i></a></li>
+              <li id="find-me"><i class="material-icons">location_searching</i></li>
               <li><a href="#"><i class="material-icons">comment</i></a></li>
               <li><a href="aide"><i class="material-icons">help_outline</i></a></li>
               <li><a href="users"><i class="material-icons">person</i></a></li>
@@ -51,6 +55,8 @@
           </nav>
         </footer>
 
-
+        <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+        {{ Html::script('../public/js/map.js') }}
+        <script>Maps.initMap();</script>
     </body>
 </html>
