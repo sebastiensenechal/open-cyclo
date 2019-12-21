0a820d81084026910d5fcf0a9657beae8e3807d0
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-12-21 11:26:27 +0100

    Seeders UsersTableSeeder and PostTableSeeder. And command php artisan db:seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+  	{
+    		DB::table('users')->delete();
+
+        // Creation de 10 utilisateurs
+    		for($i = 0; $i < 10; ++$i)
+    		{
+      			DB::table('users')->insert([
+      				'name' => 'Nom' . $i,
+      				'email' => 'email' . $i . '@blop.fr',
+      				'password' => bcrypt('password' . $i),
+      				'admin' => rand(0, 1)
+      			]);
+    		}
+  	}
+}
