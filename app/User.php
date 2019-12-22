e214defe438494edb4bda9855ec1a21d39cff5a4
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-12-22 01:26:20 +0100

    Post model and relation 1:n with User model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -39,6 +40,11 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 
 
 }
