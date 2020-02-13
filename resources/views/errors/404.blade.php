1e9f6a8965979a1b62efe1e86bd8fcea31db495c
Sébastien Sénéchal <sebast.senechal@gmail.com>
2020-02-13 11:29:31 +0100

    Change message on 404 and 403 page. Fix style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.template')
 
 @section('titre')
-Il y a un problème !
+	Page introuvable
 @endsection
 
 @section('sous-titre')
-<p>Mais tout n'est pas perdu, reprenez votre navigation.</p>
+	<p>Mais tout n'est pas perdu, reprenez votre navigation.</p>
 @endsection
 
 @section('contenu')
 
-<section class="form-center">
-	<p>Nous sommes désolés mais la page que vous désirez n'existe pas...</p>
-</section>
+	<section class="form-center">
+		<p>Nous sommes désolés mais la page que vous désirez n'existe pas...</p>
+	</section>
 
 @endsection
--- resources/views/errors/pdo.blade.php
+++ resources/views/errors/pdo.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.template')
 
 @section('titre')
-Il y a un problème !
+	Un problème s'est produit
 @endsection
 
 @section('sous-titre')
-<p>Mais tout n'est pas perdu, reprenez votre navigation.</p>
+	<p>Mais tout n'est pas perdu, reprenez votre navigation.</p>
 @endsection
 
 @section('contenu')
 
-<section class="form-center">
-	<p>Notre base de données semble inaccessible pour le moment.</p>
-	<p>Veuillez nous en excuser.</p>
-</section>
+	<section class="form-center">
+		<p>Notre base de données semble inaccessible pour le moment.</p>
+		<p>Veuillez nous en excuser.</p>
+	</section>
 
 @endsection
