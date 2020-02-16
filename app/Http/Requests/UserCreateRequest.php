f7c3b2ac0e1f0d3d07e239a4c0d6109cbc2d1f59
Sébastien Sénéchal <sebast.senechal@gmail.com>
2020-02-16 12:41:28 +0100

    Indent

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -8,26 +8,26 @@ use App\Http\Requests\Request;
 class UserCreateRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+    * Determine if the user is authorized to make this request.
+    *
+    * @return bool
+    */
     public function authorize()
     {
         return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
+    * Get the validation rules that apply to the request.
+    *
+    * @return array
+    */
     public function rules()
     {
         return [
-          'name' => 'required|max:255|unique:users',
-          'email' => 'required|email|max:255|unique:users',
-          'password' => 'required|confirmed|min:6'
+            'name' => 'required|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6'
         ];
     }
 }
